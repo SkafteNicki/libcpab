983b1bc1e2cc166c2d67a0588e0d006fc8235031
Nicki Skafte <skaftenicki@gmail.com>
2018-08-08 07:42:10 +0200

    update

--- cpab_lib/__init__.py
+++ cpab_lib/__init__.py
@@ -5,4 +5,28 @@ Created on Thu Jul 12 16:02:06 2018
 @author: nsde
 """
 
+#%%
 from .cpab import cpab
+from . import helper
+from . import cpab1d
+from . import cpab2d
+from . import cpab3d
+from sys import platform as _platform
+from .helper.utility import check_for_gpu as _check_for_gpu
+from .helper.utility import check_cuda_support as _check_cuda_support
+
+#%%
+# Check if we can use the slow or the fast version of the cpab implementation 
+# in 2D and 3D
+
+print(70*'-')
+print('Operating system:', _platform)
+
+_gpu = _check_for_gpu() and _check_cuda_support()
+if (_platform == "linux" or _platform == "linux2" \
+    or _platform == "darwin") and _gpu: 
+   print('Using the fast cuda implementation for CPAB in 2D and 3D')
+else:
+   # Windows 32 or 64-bit or no GPU
+   print('Using the slow pure tensorflow implementation for CPAB in 2D and 3D')
+print(70*'-')
--- cpab_lib/cpab.py
+++ cpab_lib/cpab.py
@@ -4,8 +4,10 @@ Created on Thu Jul 12 16:02:06 2018
 
 @author: nsde
 """
+#%%
+from .helper.utility import get_dir
 
-
+#%%
 class cpab:
     '''
     
@@ -13,8 +15,46 @@ class cpab:
     def __init__(self, tess_size, 
                  zero_boundary=False, 
                  volume_perservation=False):
-        pass
-    
+        # Check input
+        assert len(tess_size) > 0 and len(tess_size) <= 3, \
+            '''Transformer only support 1D, 2D or 3D'''
+        assert type(tess_size) == list or type(tess_size) == tuple, \
+            '''Argument tess_size must be a list or tuple'''
+        assert all([type(e)==int for e in tess_size]), \
+            '''All elements of tess_size must be integers'''
+        assert all([e > 0 for e in tess_size]), \
+            '''All elements of tess_size must be positive'''
+        assert type(zero_boundary) == bool, \
+            '''Argument zero_boundary must be True or False'''
+        assert type(volume_perservation) == bool, \
+            '''Argument volume_perservation must be True or False'''
+        
+        # Parameters
+        self.nc = tess_size
+        self.ndim = len(tess_size)
+        self.valid_outside = not(zero_boundary)
+        self.zero_boundary = zero_boundary
+        self.volume_perservation = volume_perservation
+        self.domain_max = [1 for e in self.nc]
+        self.domain_min = [-1 for e in self.nc]
+        self.inc = [(self.domain_max[i] - self.domain_min[i]) / 
+                    self.nc[i] for i in range(self.ndim)]
+        self.basis_name = 'cpab_basis_dim' + str(self.ndim) + '_tess' + \
+                          '_'.join([str(e) for e in self.nc]) + '_' + \
+                          'vo' + str(int(self.valid_outside)) + '_' + \
+                          'zb' + str(int(self.zero_boundary)) + '_' + \
+                          'vp' + str(int(self.volume_perservation))
+        self.basis_file = get_dir(__file__) + '/../' + self.basis_name
+        
     def transform(self):
         pass
     
+    def sample_grid(self):
+        pass
+    
+    def sample_transformation(self):
+        pass
+
+#%%
+if __name__ == '__main__':
+    transformer = cpab(tess_size=(2,3,4))
