24c975c2d8b802a1b542115cf116266afd768993
Nicki Skafte <skaftenicki@gmail.com>
2018-08-28 14:14:38 +0200

    working on changing the repo structure

--- repo_status.py
+++ repo_status.py
@@ -5,29 +5,44 @@ Created on Tue Aug  7 13:50:53 2018
 @author: nsde
 """
 
-# TODO: test interpolation for 3D
+#%%
+# Future repo structure
+#   -libcpab/
+#       *tensorflow/
+#           +cpab1d/
+#               -...
+#           +cpab2d/
+#               -...
+#           +cpab3d/
+#               -...
+#           +helper/
+#               -...
+#           +__init__.py
+#           +cpab.py
+#       *pytorch/
+#           +__init__.py
+#           +cpab.py
+#           +helper.py
+#       *helper/
+#           +utility.py
+#           +math.py
+#       *basis_files/
+#           +...
+#   -data/
+#       *...
+#   -demo1.py
+#   -demo2.py
+#   -demo3.py
+#
+# Future import structure
+# from libcpab import tf_cpab as cpab
+# or
+# from libcpab import torch_cpab as cpab
+#
 
 #%%
 if __name__ == '__main__':
     from libcpab.develop.cpab import cpab
-    import matplotlib.pyplot as plt
-    import scipy.io
-    import numpy as np
-    import torch
-    
-    data = scipy.io.loadmat('data/mri_scan.mat')['D']
-    data = torch.Tensor(data)[None,None,:,:,:].permute(0,1,4,3,2)
-    T = cpab([2,2,2])
-    g = T.uniform_meshgrid([128, 128, 27])
-    theta = T.identity(1)
-    gt = T.transform_grid(g, theta)
-    data_new = T.interpolate(data, gt, outsize=(128, 128, 27))
-#    im_new = T.transform_data(im2, theta, outsize=(350, 350))
-#    image = im_new.permute(0,2,3,1)
-#    image = image - image.min()
-#    image = image / image.max()
-#    
-#    plt.imshow(image[0].numpy())
     
     
     
