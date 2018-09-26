990865bc96da5a533c68d86bc2d39ec6f892929a
Nicki Skafte <skaftenicki@gmail.com>
2018-09-26 11:54:08 +0200

    initial commit for the fast pytorch version

---
+++ libcpab/pytorch/transformer/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 11:46:30 2018
+
+@author: nsde
+"""
+
---
+++ libcpab/pytorch/transformer/transformer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 11:46:55 2018
+
+@author: nsde
+"""
