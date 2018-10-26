10afab24282f55760d4388b73ae21e166ede4a67
Soren Hauberg <sohau@dtu.dk>
2018-10-26 07:56:51 +0200

    change include path to point to shared code

--- libcpab/numpy/src/npcpab.cc
+++ libcpab/numpy/src/npcpab.cc
@@ -2,7 +2,7 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 #include <math.h>
-#include "cpabcpu.cc"
+#include "../../shared/cpabcpu.cc"
 
 /*****************************************************************************
  * Array access macros.                                                      *
